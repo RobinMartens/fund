5ddaf7c0ddd5da9c8888ce71345ab6410b3605cc
Robin Martens <robin_martens@brown.edu>
2014-05-03 15:32:01 -0400

    add errors to stack

--- stack.go
+++ stack.go
@@ -1,5 +1,9 @@
 package fund
 
+import (
+    "errors"
+)
+
 type Elm struct {
     val int
     rest *Elm
@@ -9,15 +13,19 @@ type Stack struct {
     top *Elm
 }
 
-func (s *Stack) Push(val int) {
+func (s *Stack) Push(val int) error {
     e := new(Elm)
     e.val = val
     e.rest = s.top
     s.top = e
+    return nil
 }
 
-func (s *Stack) Pop() int {
+func (s *Stack) Pop() (int, error) {
     e := s.top
+    if e == nil {
+       return 0, errors.New("Stack empty!")
+    }
     s.top = e.rest
-    return e.val
+    return e.val, nil
 }

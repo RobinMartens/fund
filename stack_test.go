2133f092154c173fff368b5335ea688130f02384
Robin Martens <robin_martens@brown.edu>
2014-05-03 21:28:14 -0400

    successfully test stack and stack_test

--- stack_test.go
+++ stack_test.go
@@ -4,24 +4,35 @@ import (
     "testing"
 )
 
-func TestStack(t *testing.T) {
-
-    in0 := 13
-    in1 := 42
-    in2 := 273
-
+func TestPush(t *testing.T) {
     s := new(Stack)
-    _ = s.Push(in0)
-    _ = s.Push(in1)
-    _ = s.Push(in2)
-
-    out0, _ := s.Pop()
-    out1, _ := s.Pop()
-    out2, _ := s.Pop()
-
-    if in0 != out2 || in1 != out1 || in2 != out0 {
-       t.Errorf("Error!")
+    in := []int{1,2,3,4,5,6,7,8,9,10}
+    for i := 0; i < 10; i++ {
+        err := s.Push(in[i])
+        if err != nil {
+            t.Errorf("Cannot push value %d", in[i]);
+        }
     }
-
-
 }
+
+func TestPop(t *testing.T) {
+    s := new(Stack)
+    in := []int{1,2,3,4,5,6,7,8,9,10}
+    out := []int{10,9,8,7,6,5,4,3,2,1}
+    for i := 0; i < 10; i++ {
+        err := s.Push(in[i])
+        if err != nil {
+            t.Errorf("Cannot push value %d", in[i]);
+        }
+    }
+    for i := 0; i< 10; i++ {
+        val, err := s.Pop()
+        if val != out[i] {
+            t.Errorf("Popped value %d does not agree with expected value %d", val, out[i])
+        }
+        if err != nil {
+            t.Errorf("Cannot pop %d. value", i)
+        }
+    }
+}
+

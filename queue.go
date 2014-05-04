95f76f3ef04740124531e70d45f60d5f831c52a1
Robin Martens <robin_martens@brown.edu>
2014-05-03 21:51:28 -0400

    queue tests passed

--- queue.go
+++ queue.go
@@ -12,30 +12,33 @@ type Queue struct {
 }
 
 func (q *Queue) Enqueue(val int) error {
-    err := in.Push(val)
-    n_in++
+    err := q.in.Push(val)
+    q.n_in++
     if err != nil {
-        errors.New("Cannot push value to in stack")
+        return errors.New("Cannot push value to in stack")
     }
+    return nil
 }
 
 func (q *Queue) Dequeue() (int, error) {
-    if n_out == 0 {
-        for n_in > 0; n_in-- {
-            inErr, e := in.Pop()
-            outErr = out.Push(e)
-            n_out++
+    if q.n_out == 0 {
+        for ; q.n_in > 0; q.n_in-- {
+            e, inErr := q.in.Pop()
+            outErr := q.out.Push(e)
+            q.n_out++
             if inErr != nil {
-                errors.New("Cannot pop from in stack")
+                return 0, errors.New("Cannot pop from in stack")
             }
             if outErr != nil {
-                errors.New("Cannot push to out stack")
+                return 0, errors.New("Cannot push to out stack")
             }
-        {
+        }
     }
-    err, val := out.Pop()
-    n_out--
+    val, err := q.out.Pop()
+    q.n_out--
     if err != nil {
         errors.New("Cannot pop from out stack")
     }
+    return val, nil
 }
+
